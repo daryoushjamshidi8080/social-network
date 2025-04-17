3a3901c1deecd7ed6473d0867de134c33c0f8efe
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-17 15:43:02 +0330

    create form for register and set style for forms

---
+++ project/account/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your username'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Your Email..'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Your password ..'}))
