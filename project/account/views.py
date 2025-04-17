4fd6b9a6c36a26de6bea172b1533b6c2524c471e
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-17 19:23:44 +0330

    create get info user in registertion form and save user

--- project/account/views.py
+++ project/account/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 class RegisterView(View):
-    def get(self, request):
-        form = UserRegistrationForm()
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
 
-        return render(request, 'account/register.html', {'form': form})
+    def get(self, request):
+        form = self.form_class()
+
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        pass
+
+        form = self.form_class(request.POST)
+        print('hi')
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(
+                cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'You registered successfully', 'success')
+
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
