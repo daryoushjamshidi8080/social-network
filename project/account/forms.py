968bf70f133290d403b8d73ffd4a472779ce794f
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-18 19:53:28 +0330

    comlpete style login page

--- project/account/forms.py
+++ project/account/forms.py
@@ -45,5 +45,7 @@ class UserRegistrationForm(forms.Form):
 
 class UserLoginForm(forms.Form):
 
-    username = forms.CharField()
-    password = forms.CharField()
+    username = forms.CharField(label='', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(
+        label='', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
