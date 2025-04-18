7df5fd6feb05a32e6608befb885e705c4a194be0
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-18 18:25:21 +0330

    set comfirm password and complete process validation  passwrod and comfirm password

--- project/account/forms.py
+++ project/account/forms.py
@@ -9,7 +9,10 @@ class UserRegistrationForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(
         attrs={'class': 'form-control', 'placeholder': 'Your Email..'}))
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Your password ..'}))
+        label='password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Your password ..'}))
+
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Your Confirm password ..'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -29,3 +32,12 @@ class UserRegistrationForm(forms.Form):
             raise ValidationError('This username already exists')
 
         return username
+
+    def clean(self):
+        cd = super().clean()
+
+        password = cd.get('password')
+        confirm_password = cd.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', 'Password must match')
