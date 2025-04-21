1ad44151bd586a12052a5544b47a44c2fb1fa1f0
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-21 20:29:43 +0330

    show detail post

--- project/home/views.py
+++ project/home/views.py
@@ -7,3 +7,10 @@ class HomeView(View):
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'home/index.html', {'posts': posts})
+
+
+class PostDetailView(View):
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk=post_id, slug=post_slug)
+
+        return render(request, 'home/detail_post.html', {'post': post})
