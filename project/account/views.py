5c42a7afda11b033e41c13b37d0555c4c54d080e
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-25 16:17:59 +0330

    Add password reset system with Django built-in views and email support

--- project/account/views.py
+++ project/account/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.exceptions import ValidationError
 from django.contrib.auth.mixins import LoginRequiredMixin
 from post.models import Post
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 
 class UserRegisterView(View):
@@ -81,3 +83,22 @@ class UserProfileView(LoginRequiredMixin, View):
         user = User.objects.get(pk=user_id)
         posts = Post.objects.filter(user_id=user_id)
         return render(request, 'account/profile.html', {'user': user, 'posts': posts, 'user_id': user_id})
+
+
+class UserPasswordResetView(auth_views.PasswordResetView):
+    template_name = 'account/password_reset_form.html'
+    success_url = reverse_lazy('account:password_reset_done')
+    email_template_name = 'account/password_reset_email.html'
+
+
+class UserPasswordResetDoneView(auth_views.PasswordResetDoneView):
+    template_name = 'account/password_reset_done.html'
+
+
+class UserPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = 'account/password_reset_confirm.html'
+    success_url = reverse_lazy('account:password_reset_complete')
+
+
+class UserPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name = 'account/password_reset_complete.html'
