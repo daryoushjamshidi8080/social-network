50ebacf31a260712d57165c80319adb5bc16621b
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-26 20:35:47 +0330

    add related name for model post

--- project/post/models.py
+++ project/post/models.py
@@ -6,11 +6,15 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
     body = models.TextField()
     slug = models.SlugField()
     create = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-body',)
 
     def __str__(self):
         return f' {self.slug}  "{self.update}"'
