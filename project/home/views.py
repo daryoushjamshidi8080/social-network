fcfcbe0f0ad6e81679904bc04084091ab1db86f9
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-04-28 17:31:59 +0330

    set decorator login required for post method

--- project/home/views.py
+++ project/home/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from post.models import Post
 from post.froms import CommentCreateFrom
+from django.contrib import messages
 
 
 class HomeView(View):
@@ -11,11 +12,29 @@ class HomeView(View):
 
 
 class PostDetailView(View):
+    form_class = CommentCreateFrom
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(
+            Post, pk=kwargs['post_id'], slug=kwargs['post_slug'])
+
+        return super().setup(request, *args, **kwargs)
 
     def get(self, request, post_id, post_slug):
-        form = CommentCreateFrom()
+        form = self.form_class()
 
-        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
-        comments = post.pcomments.filter(is_reply=False)
+        comments = self.post_instance.pcomments.filter(is_reply=False)
 
-        return render(request, 'post/detail_post.html', {'post': post, 'comments': comments, 'form': form})
+        return render(request, 'post/detail_post.html', {'post': self.post_instance, 'comments': comments, 'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(
+                request, 'Your comment submitted successfully', 'success')
+            return redirect('home:post_detail', self.post_instance.id, self.post_instance.slug)
